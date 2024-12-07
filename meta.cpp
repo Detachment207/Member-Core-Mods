6e15414fd55cd7befc8bbed30e97054486e13541
Edward <132046521+Huenik@users.noreply.github.com>
2024-12-07 15:12:46 +1030

    A

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 3378916274;
+name = "D207 Publication of Member Contributitions - Core Mods";
+timestamp = ;
---
+++ mod.cpp
@@ -0,0 +1,5 @@
+name = "D207 Publication of Member Contributitions - Core Mods";
+
+logo = "Logos\iconSmall.paa";
+logoSmall = "Logos\iconSmall.paa";
+logoOver = "Logos\iconSmall.paa";
